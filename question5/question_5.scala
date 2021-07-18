32f07db9858494b3dcaacb867eb27340d8608f91
MadhuniT <2019cs168@stu.ucsc.lk>
2021-07-18 17:28:32 +0530

    AddEven

---
+++ question5/question_5.scala
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import scala.io.StdIn.readInt;
+
+object question_05{
+
+	def isEven(m:Int): Boolean=m match{
+        	case 0 => true
+		case _ => isOdd(m-1)
+   	}
+	
+	def isOdd(m:Int): Boolean={
+     		! (isEven(m)) 
+   	}
+	
+	def add(m:Int, n:Int=0 ,res:Int=0) : Int =n match{
+		case x if (x==m) => res
+		case x if ((x<m) && (isOdd(n))) => add(m,n+1,res)
+		case x if ((x<m) && (! isOdd(n))) => add(m,n+1,res+n)
+	}
+
+	def main(args:Array[String]){
+		
+		var input = new Scanner (System.in);
+		print("Enter number :");
+	  val num=input.nextInt();
+	  print("get the addition of all even numbers less than given number\n");
+
+		println(add(num));
+	}
+}
+
